4718c752004b6a4a8ec250337622170a5bc2ac6b
zhangggg217 <z13389271787@qq.com>
2025-03-02 16:42:15 +0800

    增加了几个文件

--- openjudge/8209/2400010773.py
+++ openjudge/8209/2400010773.py
@@ -1,37 +1,3 @@
-#平凡的建树思路，空间超了
-N,M=map(int,input().split())
-nums=[int(input()) for _ in range(N)]
-class TreeNode():
-    def __init__(self,divided,index):
-        self.divided=divided
-        self.index=index
-
-from collections import deque
-queue=deque([TreeNode([nums[0]],0)])
-minimum=10000
-while queue:
-
-    node=queue.popleft()
- 
-
-    if node.index<N-1:
-        new_num=nums[node.index+1]
-        tmp_divided=node.divided[:]
-        tmp_divided.append(new_num)
-
-        leftnode=TreeNode(tmp_divided,node.index+1)
-        
-        queue.append(leftnode)
-        tmp_divided=node.divided[:]
-        tmp_divided[-1]+=new_num
-        rightnode=TreeNode(tmp_divided,node.index+1)
-        queue.append(rightnode)
-    if len(node.divided)==M and node.index==N-1:
-        tmp=max(node.divided)
-        minimum=min(minimum,tmp)
-print(minimum)
-
-
 #二分查找，注意到M小N大，我们对M二分枚举（类似于aggressive cows）
 
 def min_max_expense(N, M, expenses):
